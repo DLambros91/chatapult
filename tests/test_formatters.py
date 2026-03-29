483e3997d0477a5e41c438a5931039d78cbea9ff
Shreyas G Gowda <shreyasg2526@gmail.com>
2026-03-29 22:12:26 +0530

    feat: add mention_all and mention_user helper functions

---
+++ tests/test_formatters.py
@@ -0,0 +1,22 @@
+# SPDX-License-Identifier: Apache-2.0
+"""
+Unit tests for chatapult.formatters helper functions.
+"""
+import pytest
+from chatapult.formatters import mention_all, mention_user
+
+
+def test_mention_all():
+    """mention_all() should return <users/all>"""
+    assert mention_all() == "<users/all>"
+
+
+def test_mention_user():
+    """mention_user() should return <users/{user_id}>"""
+    assert mention_user("12345") == "<users/12345>"
+
+
+def test_mention_user_different_ids():
+    """mention_user() should work with any user ID."""
+    assert mention_user("abc") == "<users/abc>"
+    assert mention_user("99999") == "<users/99999>"
